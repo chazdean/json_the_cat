fefd4ba31f0143e39e8b490af3597343f0618203
chazdean <chaz.dean@shopify.com>
2021-08-20 23:35:35 +0000

    breed search functional using cat api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,21 @@
 const request = require('request');
+const userInput = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error, response, body) => {
+ 
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  console.log(data);
+
+  if (data.length === 0) {
+    console.log("Breed Not Found");
+    return;
+  }
+  
+  console.log(data[0].description);
+});
+
