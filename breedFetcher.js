be59a4dc7d6a348292e74331963eb54168bdb577
chazdean <chaz.dean@shopify.com>
2021-08-21 04:49:44 +0000

    removed idle code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,5 @@
 const request = require('request');
 
-/* const findDescription = (data) => {
-  if (data.length === 0) {
-    return "Breed Not Found";
-  } else {
-    return data[0].description;
-  }
-}; */
-
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     
