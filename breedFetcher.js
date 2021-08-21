fd116704a1086a71b79c444189d4664ddc2a0be0
chazdean <chaz.dean@shopify.com>
2021-08-21 04:48:35 +0000

    removed helper function and edited error callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,26 @@
 const request = require('request');
 
-const findDescription = (data) => {
+/* const findDescription = (data) => {
   if (data.length === 0) {
     return "Breed Not Found";
   } else {
     return data[0].description;
   }
-};
+}; */
 
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-
-    if (!error) {
-      const data = JSON.parse(body);
-      callback(null, findDescription(data));
-    } else {
+    
+    if (error || response.statusCode !== 200) {
       callback(error, null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    if (data.length !== 0) {
+      callback(null, data[0].description);
+    } else {
+      callback("Breed Not Found", null);
     }
   });
 };
